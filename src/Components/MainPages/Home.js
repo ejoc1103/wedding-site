88add55c94a79afc721b9d4d0561d03ffc75ea61
Ed OConnor <ejoc1103@yahoo.com>
2023-01-19 15:03:56 -0500

    Added time info and registry links

--- src/Components/MainPages/Home.js
+++ src/Components/MainPages/Home.js
@@ -1,54 +1,59 @@
-import "../../App.css";
-import mainPhoto from "../../images/mainPhoto.jpg";
-import venue from "../../images/reception.jpg";
-import after from "../../images/river-rock.jpg";
+import '../../App.css';
+import mainPhoto from '../../images/mainPhoto.jpg';
+import venue from '../../images/reception.jpg';
+import after from '../../images/river-rock.jpg';
 
 function Home() {
   return (
     <>
       <div></div>
-      <img className="main-photo" src={mainPhoto} alt="main couple shot" />
+      <img className='main-photo' src={mainPhoto} alt='main couple shot' />
       <div></div>
-      <div className="wedding-info">
-        <h1 className="wedding-day">Wedding Day</h1>
-        <h2>July 23rd, 2023</h2>
-        <h2>Times still TBD</h2>
+      <div className='wedding-info'>
+        <h1 className='wedding-day'>Wedding Day</h1>
+        <h1>July 23rd, 2023</h1>
+        <h1>
+          Ceremony <br /> Starts at 4:30 pm
+        </h1>
       </div>
       <div></div>
-      <hr className="main-hr" />
+      <hr className='main-hr' />
       <div></div>
-      <h1 className="reception-header">Ceremony and Reception</h1>
-      <div></div>
-      <div></div>
-      <img className="main-photo" src={venue} alt="Ceremony Location" />
-      <div></div>
-      <h1 className="wedding-day">Crystall Point Ballroom</h1>
+      <h1 className='reception-header'>Ceremony and Reception</h1>
       <div></div>
       <div></div>
-      <div className="wedding-info">
-        <div className="event-info">
-          <h2>Times still TBD</h2>
+      <img className='main-photo' src={venue} alt='Ceremony Location' />
+      <div></div>
+      <h1 className='wedding-day'>Crystall Point Ballroom</h1>
+      <div></div>
+      <div></div>
+      <div className='wedding-info'>
+        <div className='event-info'>
+          <h2>
+            Cocktail Hour <br /> From 5 pm to 6 pm <br /> Reception <br /> From
+            6 pm to 10 pm
+          </h2>
           <h2>3900 River Rd, Point Pleasant, NJ 08742</h2>
         </div>
       </div>
       <div></div>
-      <hr className="main-hr" />
+      <hr className='main-hr' />
       <div></div>
-      <h1 className="reception-header">After Party </h1>
+      <h1 className='reception-header'>After Party </h1>
       <div></div>
       <div></div>
-      <img className="main-photo" src={after} alt="Ceremony Location" />
+      <img className='main-photo' src={after} alt='Ceremony Location' />
       <div></div>
 
-      <h1 className="wedding-day">The River Rock</h1>
+      <h1 className='wedding-day'>The River Rock</h1>
       <div></div>
       <div></div>
-      <div className="wedding-info">
-        <div className="event-info">
-          <h2 className="bottom-info">Times still TBD</h2>
-          <h2 className="bottom-info">1600 NJ-70, Brick Township, NJ 08724</h2>
+      <div className='wedding-info'>
+        <div className='event-info'>
+          <h2 className='bottom-info'>10pm - Midnight</h2>
+          <h2 className='bottom-info'>1600 NJ-70, Brick Township, NJ 08724</h2>
           <div></div>
-          <h2 className="bottom-info">Right Across the Street</h2>
+          <h2 className='bottom-info'>Right Across the Street</h2>
         </div>
       </div>
       <div></div>
--- src/Components/MainPages/Registry.js
+++ src/Components/MainPages/Registry.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React from 'react';
 
-import hawaii from "../../images/hawaii.jpg";
-
+import puertoRico from '../../images/puertoRico.jpg';
+import registry from '../../images/registry.png';
 export default function Registry() {
   return (
     <>
       <div></div>
-      <div className="registry-container">
-        <h1 className="registry-header">Registry Options</h1>
-        <hr className="main-hr" />
-        <div className="registry-child">
-          <div className="wedding-info">
-            <h1 className="wedding-day">Donate to Our Honeymoon Fund Here</h1>
+      <div className='registry-container'>
+        <h1 className='registry-header'>Registry Options</h1>
+        <hr className='main-hr' />
+        <div className='registry-child'>
+          <div className='wedding-info'>
+            <h1 className='wedding-day'>
+              Donate to Our Puerto Rico Honeymoon Fund Here
+            </h1>
           </div>
           <a
-            className="regular-links"
-            href="https://registry.theknot.com/ed-oconnor-shelly-applegate-july-2023-nj/41696237"
-            target="_blank"
-            rel="noreferrer"
+            className='regular-links'
+            href='https://registry.theknot.com/ed-oconnor-shelly-applegate-july-2023-nj/41696237'
+            target='_blank'
+            rel='noreferrer'
           >
-            <img className="honeymoon-photo" src={hawaii} alt="honey moon" />
+            <img
+              className='honeymoon-photo'
+              src={puertoRico}
+              alt='honey moon'
+            />
             <h3>Click Here!</h3>
           </a>
         </div>
-        <hr className="registry-hr" id="registry-sc-hr" />
-        <h1 className="registry-child">
-          Bed Bath and Beyond Registry Coming Soon!
-        </h1>
+        <hr className='registry-hr' id='registry-sc-hr' />
+        <div className='registry-child'>
+          <div className='wedding-info'>
+            <h1 className='wedding-day'>
+              Bed Bath and Beyond Registry Coming Soon!
+            </h1>
+          </div>
+          <a
+            className='regular-links'
+            href='https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/552036724'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img className='honeymoon-photo' src={registry} alt='registry' />
+            <h3>Click Here!</h3>
+          </a>
+        </div>
       </div>
       <div></div>
     </>
