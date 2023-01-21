4d8015c78507e42b601e68ecda810270a4f75f63
Ed OConnor <ejoc1103@yahoo.com>
2023-01-21 15:54:59 -0500

    Fixed some styling issues with the navigation menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Components/Utilities/Header";
-import Nav from "./Components/Utilities/Nav";
-import Footer from "./Components/Utilities/Footer";
-import Photos from "./Components/MainPages/Photos";
-import Travel from "./Components/MainPages/Travel";
-import Registry from "./Components/MainPages/Registry";
-import Home from "./Components/MainPages/Home";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Utilities/Header';
+import Nav from './Components/Utilities/Nav';
+import Footer from './Components/Utilities/Footer';
+import Photos from './Components/MainPages/Photos';
+import Travel from './Components/MainPages/Travel';
+import Registry from './Components/MainPages/Registry';
+import Home from './Components/MainPages/Home';
+import RSVP from './Components/MainPages/RSVP';
 
 function App() {
   return (
-    <div className="main">
+    <div className='main'>
       <Router>
         <Header />
         <Nav />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/photos" element={<Photos />} />
-          <Route path="/travel" element={<Travel />} />
-          <Route path="/registry" element={<Registry />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/photos' element={<Photos />} />
+          <Route path='/travel' element={<Travel />} />
+          <Route path='/registry' element={<Registry />} />
+          {/* <Route path='/rsvp' element={<RSVP />} /> */}
         </Routes>
         <Footer />
       </Router>
--- src/Components/Utilities/Nav.js
+++ src/Components/Utilities/Nav.js
@@ -1,61 +1,70 @@
-import React from "react";
-import "../../App.css";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import '../../App.css';
+import { NavLink, useLocation } from 'react-router-dom';
+
 const Nav = () => {
+  const location = useLocation();
   return (
     <>
       <div></div>
-      <div className="nav">
-        <ul className="nav-list">
+      <div className='nav'>
+        <ul className='nav-list'>
           <NavLink
-            className={isActive =>
-              "nav-link" + (!isActive ? " unselected" : "")
+            className={
+              'nav-link' + (location.pathname === '/' ? ' active' : '')
             }
             end
-            to="/"
-            id="homeTab"
+            to='/'
+            id='homeTab'
           >
             Home
           </NavLink>
           <NavLink
-            className={isActive =>
-              "nav-link" + (!isActive ? " unselected" : "")
+            className={
+              'nav-link' + (location.pathname === '/photos' ? ' active' : '')
             }
-            to="/photos"
+            to='/photos'
           >
             Photos
           </NavLink>
           <NavLink
-            className={isActive =>
-              "nav-link" + (!isActive ? " unselected" : "")
+            className={
+              'nav-link' + (location.pathname === '/travel' ? ' active' : '')
             }
-            to="/travel"
+            to='/travel'
           >
             Travel
           </NavLink>
           <NavLink
-            className={isActive =>
-              "nav-link" + (!isActive ? " unselected" : "")
+            className={
+              'nav-link' + (location.pathname === '/registry' ? ' active' : '')
             }
-            to="/registry"
+            to='/registry'
           >
             Registry
           </NavLink>
           <a
-            target="_blank"
-            rel="noreferrer"
-            activeclassname="is-active"
-            className="nav-link"
-            href="https://www.theknot.com/us/shelly-applegate-and-ed-o-connor-jul-2023/rsvp"
+            target='_blank'
+            rel='noreferrer'
+            activeclassname='is-active'
+            className='nav-link'
+            href='https://www.theknot.com/us/shelly-applegate-and-ed-o-connor-jul-2023/rsvp'
           >
             RSVP
           </a>
         </ul>
       </div>
       <div></div>
-      <hr className="main-hr" />
+      <hr className='main-hr' />
     </>
   );
 };
 
 export default Nav;
+
+{/* <NavLink
+  className={'nav-link' + (location.pathname === '/rsvp' ? ' active' : '')}
+  to='/rsvp'
+>
+  RSVP
+</NavLink>; */}
